9e9f24d8de4ae371af5811fcbb90cbb9dd12c7d9
Wade Austin <wade.austin@gmail.com>
2015-02-09 09:31:15 -0600

    update version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-text-blocks',
-    version='0.1',
+    version='0.2',
     packages=['text_blocks'],
     include_package_data=True,
     license='BSD License',  # example license
